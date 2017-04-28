94996e4e941dba403f1fda6f403631064c05458b
eliogsolis <elio.gsolis@gmail.com>
2017-04-28 15:35:57 -0500

    etiqueta activa sidebar

--- src/views/sidebar.blade.php
+++ src/views/sidebar.blade.php
@@ -1,27 +1,18 @@
 @php
     $value = config('sidebar.menu');
-    // $url = Route::currentRouteName();
-    // $prefix = explode('.' , $url);
+    $url = Route::currentRouteName();
+    $prefix = explode('.' , $url);
 @endphp
 
 <div class="page-sidebar sidebar">
     <div class="page-sidebar-inner slimscroll">
-       {{--  <div class="sidebar-header">
-            <div class="sidebar-profile">
-                <a href="javascript:void(0);" id="profile-menu-link">
-                    <div class="sidebar-profile-image">
-                        <img src="assets/images/avatar1.png" class="img-circle img-responsive" alt="">
-                    </div>
-                    <div class="sidebar-profile-details">
-                        <span>David Green<br><small>Art Director</small></span>
-                    </div>
-                </a>
-            </div>
-        </div> --}}
         <ul class="menu accordion-menu">
         @foreach ($value as $element)
             @if ($element['type'] == 'simple')
-                <li class="active"><a href="{{ route($element['url']) }}" class="waves-effect waves-button"><span class="menu-icon glyphicon glyphicon-{{ $element['ico'] }}"></span><p> {{ $element['text'] }} </p></a></li>
+                @php
+                    $entity = explode('.' , $element['url']);
+                @endphp
+                <li class="@if ($prefix[0] == $entity[0]) active @endif"><a href="{{ route($element['url']) }}" class="waves-effect waves-button"><span class="menu-icon glyphicon glyphicon-{{ $element['ico'] }}"></span><p> {{ $element['text'] }} </p></a></li>
             @else
                 <li class="droplink"><a href="#" class="waves-effect waves-button"><span class="menu-icon glyphicon glyphicon-{{ $element['ico'] }}"></span><p> {{ $element['text'] }} </p><span class="arrow"></span></a>
                     <ul class="sub-menu">
