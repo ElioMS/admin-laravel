9741a5ccb2c9c989c74185bf35cba5fd6a51ea07
eliogsolis <elio.gsolis@gmail.com>
2017-04-28 16:41:47 -0500

    active para menu anidados

--- src/views/sidebar.blade.php
+++ src/views/sidebar.blade.php
@@ -17,7 +17,10 @@
                 <li class="droplink"><a href="#" class="waves-effect waves-button"><span class="menu-icon glyphicon glyphicon-{{ $element['ico'] }}"></span><p> {{ $element['text'] }} </p><span class="arrow"></span></a>
                     <ul class="sub-menu">
                     @foreach ($element['childs'] as $elem)
-                        <li><a href="{{ route($elem['url']) }}">{{ $elem['text'] }}</a></li>
+                        @php
+                            $entity = explode('.' , $elem['url']);
+                        @endphp
+                        <li class="child @if ($prefix[0] == $entity[0]) active @endif"><a href="{{ route($elem['url']) }}">{{ $elem['text'] }}</a></li>
                     @endforeach 
                     </ul>
                 </li>
@@ -26,3 +29,11 @@
         </ul>
     </div><!-- Page Sidebar Inner -->
 </div>
+
+<script src="https://code.jquery.com/jquery-3.2.1.js" integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE=" crossorigin="anonymous"></script>
+
+<script>
+    $(document).ready(function() {
+        $('li.active').closest('.droplink').addClass('open')
+    });
+</script>
