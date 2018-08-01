4b06a722f98030c19994a46dcdb73435aab2d4da
Elio Marcell <elio.gsolis@gmail.com>
2018-08-01 17:11:21 -0500

    helper class was removed from panel view

--- src/views/panel.blade.php
+++ src/views/panel.blade.php
@@ -305,7 +305,7 @@
                     </div>
                 </div><!-- Main Wrapper -->
                 <div class="page-footer">
-                    <p class="no-s"> <strong> Copyright </strong>&copy; {{ Helper::actualYear() }}  <a href="http://www.staffcreativa.pe/" target="_blank"><strong>Staffcreativa</strong></a>. Todos los derechos reservados.</p>
+                    {{-- <p class="no-s"> <strong> Copyright </strong>&copy; {{ Helper::actualYear() }}  <a href="http://www.staffcreativa.pe/" target="_blank"><strong>Staffcreativa</strong></a>. Todos los derechos reservados.</p> --}}
                 </div>
             </div><!-- Page Inner -->
         </main><!-- Page Content -->
